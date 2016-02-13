74d36cd1caedf19f856fe04c91479ed32fa4fee3
arturaraujo_b <arturaraujo.b@gmail.com>
2016-02-13 18:50:49 +0000

    Some updates

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -69,6 +69,6 @@ class ListsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def list_params
-      params.fetch(:list, {})
+      params.require(:list).permit(:title, :description)
     end
 end
