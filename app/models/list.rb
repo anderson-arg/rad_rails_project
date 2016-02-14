7f0ea71b75d66a7fc375d6933ef3da873839c58b
arturaraujo_b <arturaraujo.b@gmail.com>
2016-02-14 15:56:06 +0000

    Adding some valdations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ActiveRecord::Base
   acts_as_paranoid
   
+  validates :title, presence: true, length: { maximum: 140, message: "Texto muito longo. Máximo permitido: 140 caracteres." }, uniqueness: {message: "Já existe uma lista como mesmo título."}
+  
   belongs_to :user
   belongs_to :list_category
   
