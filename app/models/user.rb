3866ebd419ac7dac59961bcdd34395cf363e4510
arturaraujo_b <arturaraujo.b@gmail.com>
2016-04-02 13:57:46 +0000

    Using gem simple form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
     
     EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     
-    validates :name, :email, presence: true
+    validates :name, :email, :password, :password_confirmation, presence: true
     validates :email, format: { with:  EMAIL_REGEX }, uniqueness: true
     validates :password, :length => { :minimum => 6}, :confirmation =>true
     
