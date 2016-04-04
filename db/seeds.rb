4b0adfa5c19919cf8abe94e0fe98f536389debb9
arturaraujo_b <arturaraujo.b@gmail.com>
2016-04-04 11:20:45 +0000

    Fixing ids

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 user_1 = User.create!(:name => "Thais Correia", :email => "tnmcorreia@gmail.com", :password => "123456", :password_confirmation => '123456',  :is_active => 1)
 user_2 = User.create!(:name => "Artur Araújo", :email => "arturaraujo.b@gmail.com", :password => "123456", :password_confirmation => '123456', :is_active => 1)
 
-todo_cat = ListCategory.create(:id => 1, :title => "ToDo", :photo => "todo.png", :is_active => 1)
-bill_cat = ListCategory.create(:id => 2, :title => "Financeiro", :photo => "financial.png", :is_active => 1)
+todo_cat = ListCategory.create(:title => "ToDo", :photo => "todo.png", :is_active => 1)
+bill_cat = ListCategory.create(:title => "Financeiro", :photo => "financial.png", :is_active => 1)
 
 test_list = List.create(:list_category_id => todo_cat.id, :user => user_2, :title => "Teste", :description => "Descrição Teste", :is_active => 1)
 test2_list = List.create(:list_category_id => todo_cat.id, :user => user_2, :title => "Teste 2", :description => "Descrição Teste 2", :is_active => 1)
